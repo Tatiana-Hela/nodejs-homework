5ec831b4a75b1d65906de5afc4ec73d35d69ff27
Tatiana-Hela <malya9472@gmail.com>
2023-04-06 18:28:17 +0300

    f

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require('./app')
-
+const app = require("./app");
+// ViNZOqhcg5a1Z8tt
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
